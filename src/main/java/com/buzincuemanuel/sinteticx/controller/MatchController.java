b04b9e066b17ec91b1665ffce34e7db6f287bc7f
buzincuemanuel <emanuel.buzincu@stud.ubbcluj.ro>
2026-04-28 08:39:01 +0300

    feat: implement Match Controller

--- src/main/java/com/buzincuemanuel/sinteticx/controller/MatchController.java
+++ src/main/java/com/buzincuemanuel/sinteticx/controller/MatchController.java
@@ -1,14 +1,14 @@
 package com.buzincuemanuel.sinteticx.controller;
 
 
+import com.buzincuemanuel.sinteticx.dto.MatchDTO;
 import com.buzincuemanuel.sinteticx.model.Match;
 import com.buzincuemanuel.sinteticx.service.MatchService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,11 +20,34 @@ public class MatchController {
     private final MatchService matchService;
 
     @GetMapping
-    public ResponseEntity<List<Match>> getMatches(
+    public ResponseEntity<List<MatchDTO>> getMatches(
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size){
 
-        List<Match> matches = matchService.getPaginatedMatches(page, size);
+        List<MatchDTO> matches = matchService.getPaginatedMatches(page, size);
         return ResponseEntity.ok(matches);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Match> getMatchById(@PathVariable Long id) {
+        Match match = matchService.getMatchById(id);
+        return ResponseEntity.ok(match);
+    }
+
+    @PostMapping
+    public ResponseEntity<Match> createMatch(@Valid @RequestBody MatchDTO matchDTO) {
+        Match createdMatch = matchService.createMatch(matchDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdMatch);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Match> updateMatch(@PathVariable Long id, @Valid @RequestBody MatchDTO matchDTO) {
+        Match updatedMatch = matchService.updateMatch(id, matchDTO);
+        return ResponseEntity.ok(updatedMatch);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteMatch(@PathVariable Long id) {
+        matchService.deleteMatch(id);
+        return ResponseEntity.noContent().build();
     }
 }
